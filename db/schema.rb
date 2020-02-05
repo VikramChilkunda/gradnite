aaff0c513005d498ede009a21aee1653fead8320
EC2 Default User <ec2-user@ip-172-31-19-223.us-west-2.compute.internal>
2020-02-05 14:44:28 +0000

    mobile responsive, mostly

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200205051840) do
+ActiveRecord::Schema.define(version: 20200205072512) do
 
   create_table "buses", force: :cascade do |t|
     t.integer  "busnum"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
