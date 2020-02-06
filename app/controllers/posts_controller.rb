0bffec68a28e70841e2ca1acc2067081d8d5db89
EC2 Default User <alexgiantseed@gmail.com>
2020-02-06 04:00:17 +0000

    small heroku changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,16 @@
 class PostsController < ApplicationController
   before_action :admin_user, only: [:new]
-  
-  def new
-    @post = Post.new
-  end
-  
+
   def index
     @posts = Post.all
   end
   
   def show
     @post = Post.find(params[:id])
+  end
+  
+  def new
+    @post = Post.new
   end
   
   def create
