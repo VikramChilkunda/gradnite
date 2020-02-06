5ac4fe3796cda9b2a0a6fd5cf81e678e45f8f558
EC2 Default User <alexgiantseed@gmail.com>
2020-02-06 01:34:52 +0000

    small heroku changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
     attr_accessor :remember_token
-    before_save :default_values
 
     validates :name, presence: true, length: {maximum: 50}
     validates :idnum, presence: true, :length => { :is => 6 }, uniqueness: true
